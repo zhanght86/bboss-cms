5da52c31c6e6d17fd17a972ed644f16890d0e09d
yin-bp <yin-bp@163.com>
2015-07-16 16:40:43 +0800

    cms 频道首页指定为其他频道首页时，发布模板上的频道列表不会出现

--- src-cms/com/frameworkset/platform/cms/channelmanager/ChannelManagerImpl.java
+++ src-cms/com/frameworkset/platform/cms/channelmanager/ChannelManagerImpl.java
@@ -4169,7 +4169,8 @@ public class ChannelManagerImpl extends EventHandle implements ChannelManager {
 					.append(" and  status=0 ")
 					.append(subsql)
 					.append(" and (1=1 )")
-					.append( " and ((PAGEFLAG=0 or PAGEFLAG is null )and OUTLINE_TPL_ID is not null or (PAGEFLAG=1 or PAGEFLAG=2) and INDEXPAGEPATH is not null)")
+//					.append( " and ((PAGEFLAG=0 or PAGEFLAG is null )and OUTLINE_TPL_ID is not null or (PAGEFLAG=1 or PAGEFLAG=2) and INDEXPAGEPATH is not null)")
+					.append(" and ISNAVIGATOR=1")
 					.append(" order by order_no asc,createtime desc ").append((count <= 0 ? "" : (" ) where rownum <=" + String.valueOf(count))));
 			// System.out.println("sql=" + sql);
 			db.executeSelect(sql.toString());
@@ -4335,7 +4336,8 @@ public class ChannelManagerImpl extends EventHandle implements ChannelManager {
 					+ siteid
 					+ " and  status=0 "
 					+ subsql
-					+ " and ((PAGEFLAG=0 or PAGEFLAG is null )and OUTLINE_TPL_ID is not null or (PAGEFLAG=1 or PAGEFLAG=2) and INDEXPAGEPATH is not null)"
+//					+ " and ((PAGEFLAG=0 or PAGEFLAG is null )and OUTLINE_TPL_ID is not null or (PAGEFLAG=1 or PAGEFLAG=2) and INDEXPAGEPATH is not null)"
+					+" and ISNAVIGATOR=1" 
 					+ " order by order_no  asc,channel_id desc";
 			// System.out.println("sql=" + sql);
 			db.executeSelect(sql, offset, pageitems);
