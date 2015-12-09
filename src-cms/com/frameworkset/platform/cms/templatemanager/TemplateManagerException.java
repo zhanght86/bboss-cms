6cc2ed5db82d47d7aa22a10522599645811ff316
yin-bp <yin-bp@163.com>
2015-12-09 14:16:56 +0800

    修改jdk兼容性问题

--- src-cms/com/frameworkset/platform/cms/templatemanager/TemplateManagerException.java
+++ src-cms/com/frameworkset/platform/cms/templatemanager/TemplateManagerException.java
@@ -16,11 +16,7 @@ public class TemplateManagerException extends Exception
     	super();
     }
 
-	public TemplateManagerException(String message, Throwable cause, boolean enableSuppression,
-			boolean writableStackTrace) {
-		super(message, cause, enableSuppression, writableStackTrace);
-		// TODO Auto-generated constructor stub
-	}
+	 
 
 	public TemplateManagerException(String message, Throwable cause) {
 		super(message, cause);
