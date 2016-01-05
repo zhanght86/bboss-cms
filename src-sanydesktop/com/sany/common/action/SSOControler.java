55a9e563bc73bdaf6e9debb016776e911439622a
yin-bp <yin-bp@163.com>
2016-01-05 08:53:16 +0800

    修复用户登录设置默认语言选项时，没有将语言码写入cookie问题

--- src-sanydesktop/com/sany/common/action/SSOControler.java
+++ src-sanydesktop/com/sany/common/action/SSOControler.java
@@ -155,6 +155,10 @@ public class SSOControler {
             language = RequestContextUtils.getLocaleResolver(request).resolveLocaleCode(request);
 
         }
+        else
+        {
+        	RequestContextUtils.getLocaleResolver(request).setLocale(request, response, language);
+        }
         model.addAttribute("language", language);
 
         /*
