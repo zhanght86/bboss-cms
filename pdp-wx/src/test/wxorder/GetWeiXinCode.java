00aec0448d926516e9fafa12ed62bfd78584bdf6
dalangtaoshazi <Administrator@suwei>
2017-01-14 18:36:31 +0800

    test

--- pdp-wx/src/test/wxorder/GetWeiXinCode.java
+++ pdp-wx/src/test/wxorder/GetWeiXinCode.java
@@ -12,6 +12,7 @@ import java.net.URLEncoder;
 public class GetWeiXinCode {
 	public static String GetCodeRequest = "https://open.weixin.qq.com/connect/oauth2/authorize?appid=APPID&redirect_uri=REDIRECT_URI&response_type=code&scope=SCOPE&state=STATE#wechat_redirect";
 
+	
 	public static String getCodeRequest() {
 		String result = null;
 		GetCodeRequest = GetCodeRequest.replace("APPID", "wxe88cfaa46b73c192");
